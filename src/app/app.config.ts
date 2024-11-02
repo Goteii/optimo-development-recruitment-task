b14354eb1f3188806589a5dbee6f69dca523095f
Goteii <deyssog@gmail.com>
2024-11-02 15:40:58 +0100

    add tailwind, env blueprint, basic project structure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(appRoutes, withComponentInputBinding()),
+    // provideHttpClient(withInterceptors([authInterceptor]))
   ],
 };
